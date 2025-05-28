f96a094ace059fe7864c5e056a3a36330d42a6ef
Ghost <qpages08@gmail.com>
2025-05-28 12:14:22 +0200

    ajout

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ zombie =[[65, 17, -12,14],[80,16,-12,15],[97,17,-11,14],[113,17,-11,14]]#positio
 SKIN = 0
 
 
+
 LISTE_ENTITES =[]
 
 width = 128
@@ -24,10 +25,14 @@ pyxel.frame_count = 0#nb de frame
 x = width//2
 y = height//2
 print("self.y", y)
-JEU = "EN_COURS"
+
 PLAYER_X = 0
 PLAYER_Y = 50
 VITESSE = 3
+MODE = "DEV"
+VIE = 3
+STATUT_GAME = "PLAYING"
+
 
 def creation_mob():
         global LISTE_ENTITES
@@ -49,6 +54,23 @@ def mob(pos_x, pos_y, type_mob):
     
     pyxel.blt(pos_x, pos_y, 0, type_mob[SKIN][0], type_mob[SKIN][1], type_mob[SKIN][2],
               type_mob[SKIN][3], colkey=2)
+
+def degats_player():
+    global MODE, VIE
+    if MODE == "DEV":
+        return
+    else:
+        VIE -=1
+        if VIE == 0:
+            mort()
+
+
+
+
+def mort():
+    global STATUT_GAME
+    STATUT_GAME = "END"
+
     
     
 def update():
@@ -65,20 +87,42 @@ def update():
     if pyxel.btn(pyxel.KEY_UP):
         global PLAYER_Y, VITESSE
         PLAYER_Y += -VITESSE
+
+    if pyxel.btn(pyxel.KEY_LEFT):
+        global VIE
+        mort()
+
+     
     elif pyxel.btn(pyxel.KEY_DOWN):
         PLAYER_Y += +VITESSE
 
 
 def draw():
-    global LISTE_ENTITES
-    pyxel.cls(0)
-
-    pyxel.cls(0)
-    pyxel.blt(PLAYER_X, PLAYER_Y, 0, 0, 16, 16, 16, colkey=2, )
-    pyxel.blt(PLAYER_X-1, PLAYER_Y+5, 0, 32, 112, 16, 16, colkey=2)
+    global LISTE_ENTITES,STATUT_GAME,STAT
+    pyxel.cls(6)
     
-    for entite in LISTE_ENTITES:
-        mob(entite[0], entite[1], zombie)
+    for i in range(VIE):
+        pyxel.blt(4*(i+1), 5, 0, 115, 52, 10, 9, colkey=2)
+    
+
+    if STATUT_GAME == "PLAYING":
+
+        
+
+        
+        pyxel.blt(PLAYER_X, PLAYER_Y, 0, 0, 16, 16, 16, colkey=2, )
+        pyxel.blt(PLAYER_X-1, PLAYER_Y+5, 0, 32, 112, 16, 16, colkey=2)
+        
+        for entite in LISTE_ENTITES:
+            mob(entite[0], entite[1], zombie)
+    
+    if STATUT_GAME == "END":
+        if pyxel.frame_count % 30 < 25:  # visible pendant 15 frames sur 30
+            pyxel.text((pyxel.height-30)//2, (pyxel.width-30)//2, "You Lose", 7)
+            chaine_de_caractere = "PUSH ENTER TO START"
+            # pyxel.text((pyxel.height-114)//2, (pyxel.width-114)//2, 7)
+            pyxel.text((pyxel.width - len(chaine_de_caractere) * 4) // 2, (pyxel.height - 8) // 2, chaine_de_caractere, 7)
+
 
 
 
