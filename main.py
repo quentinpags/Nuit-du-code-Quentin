c52b0807879c4321ff43caac05e6c22ba76a8e00
skitour <aloys.poirier@gmail.com>
2025-05-28 08:39:03 +0200

    débute de classe

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pyxel
+width = 128
+height = 128
 
+pyxel.init(width, height, title='cheuvalliay')
+
+class Perso:
+    def __init__(self):
+        self.width = width
+        self.height = height
+        self.x = width//2
+        self.y = height//2
+
+
+        pyxel.run(self.update, self.draw)
+
+
+        def update():
+            pass
+
+        def draw():
+            pass
